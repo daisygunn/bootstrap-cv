9cbfbab8cda46ae85661a64f744af5c186077e2a
daisygunn <daisy.l.g@hotmail.co.uk>
2021-10-22 08:32:39 +0000

    Fixed bugs regarding empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    // empty the divs so previous data isn't shown
+    $("#gh-user-data").html("")
+    $("#gh-repo-data").html("")
     var username = $("#gh-username"). val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -67,3 +70,4 @@ function fetchGitHubInformation(event) {
         });
 }
 
+$(document).ready(fetchGitHubInformation);
