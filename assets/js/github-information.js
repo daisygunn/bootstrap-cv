751b51f3898df0111e689d39ee8e7121a21cccbd
daisygunn <daisy.l.g@hotmail.co.uk>
2021-10-22 07:48:49 +0000

    Add code to display GitHub user info

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,22 @@
+function userInformationHTML(user) {
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_black">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
+            </a>
+        </div>
+        <p>Followers: ${user.followers} - Following: ${user.following} <br>
+        Repos: ${user.public_repos}
+        </p>
+    </div>`
+}
+
 function fetchGitHubInformation(event) {
     var username = $("#gh-username"). val();
     if (!username) {
@@ -5,4 +24,23 @@ function fetchGitHubInformation(event) {
         return;
     }
     $("#gh-user-data").html(`<div id="loader"><img src="../assets/css/loader.gif" alt="loading..."></img></div>`);
+    // Promise when: something is done then: do something
+    $.when(
+        // get JSON response from github API using username entered
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        // use the response from the API to add the user info html into the div
+        function(response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+            // display error messages 404 / page not found otherwise just print the error message to the console
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").hmtl(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(console.errorResponse);
+                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
+
