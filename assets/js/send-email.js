24be9a85a1ac49ebfc6fe86afdb9caa7836b58b4
daisygunn <daisy.l.g@hotmail.co.uk>
2021-10-23 08:33:50 +0000

    Add sendMail function and connect to form

---
+++ assets/js/send-email.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm) {
+    console.log("function called")
+    emailjs.send("service_m29ys4m","project-request", {
+        "from-name": contactForm.name.value,
+        "from-email": contactForm.emailaddress.value,
+        "project-request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+
+    return false;
+}
+
+console.log("WORKING")
